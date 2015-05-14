025b95fb524275e1a5892c2447c2345182c9d3d4
= <=>
2015-05-14 19:12:04 +0200

    small changes

--- c/main.c
+++ c/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main ()
+typedef unsigned int uint;
+
+int main ( int argc, char **argv )
 {
-        printf ("Hello World\n");
+        FILE *pInfile;
+	FILE *pOutfile;
+
+	char ch, file_name[255];	
+
+	pInfile = fopen(file_name,"r");
+	pOutfile = fopen( "out.data","w");
+
+	if( fp == NULL ) {
+	      perror("Error while opening the file.\n");
+	      exit(EXIT_FAILURE);
+	   }
 
 	return 0;
 }
